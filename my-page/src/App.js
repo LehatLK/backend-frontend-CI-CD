320de16ae52c0f270d43fa05bf5db62d70a596bc
Lehat Khalid <lehatk@hotmail.se>
2023-09-21 14:56:14 +0200

    first commit

---
+++ my-page/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import styles from './App.css';
+
+function App() {
+  return (
+    <div classname="theHeader">
+      <header><h1>winter is comming</h1></header>
+      <div classname="infoDiv">Who are you looking for</div>
+    </div>
+
+
+  );
+}
+
+export default App;
